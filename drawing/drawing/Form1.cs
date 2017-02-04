796147788cac3f399c953d37f99db9e374b6513a
Stefan Mermans <stefan_mermans@hotmail.com>
2017-02-04 10:37:15 +0100

    Added some comments

--- drawing/drawing/Form1.cs
+++ drawing/drawing/Form1.cs
@@ -10,6 +10,7 @@ namespace drawing
 {
     public partial class Form1 : Form
     {
+        // List containing every particle on screen
         private List<particle> particles { get; set; } = new List<particle>();
         private List<Keys> pressed = new List<Keys>();
         private bool spawn = false;
@@ -18,16 +19,29 @@ namespace drawing
 
         public Form1()
         {
+            // Initialize the form
             InitializeComponent();
+
+            // Start the main loop timer
             System.Timers.Timer timer = new System.Timers.Timer(1000/30);
             timer.Elapsed += Timer_Tick;
             timer.Start();
+
+            // set the maximum particle count
             particles.Capacity = 10000;
+
+            // Initialize the player(particle origin)
             player = new Player(10,10);
+
             Color clr = Color.Red;
             brush = new SolidBrush(clr);
         }
 
+        /// <summary>
+        /// Timer which serves as the main loop for the program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Timer_Tick(object sender, EventArgs e)
         {
 
@@ -66,8 +80,7 @@ namespace drawing
         {
             //Anti-aliasing
             e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
-            //Anti-aliasing for text
-            //e.Graphics.TextRenderingHint = TextRenderingHint.ClearTypeGridFit;
+
             e.Graphics.DrawString(pressed.Count+" number of particle's " + particles.Count, new Font("Arial", 10), Brushes.Black, 10,
                 10);
 
@@ -91,6 +104,7 @@ namespace drawing
             Update();
         }
 
+        // Button/key handlers
         private void button1_Click(object sender, EventArgs e)
         {
             if (spawn) spawn = false;
@@ -123,17 +137,26 @@ namespace drawing
         }
     }
 
+   
     public class particle
     {
+        // The particles coordinates
         public double x { get; set; }
         public double y { get; set; }
+        // Alpha value
         public int a { get; set; }
+        // Change in direction 
         private double xd;
         private double yd;
+        // Color of the particle
         public SolidBrush brush { get; set; }
-
         private Color color;
 
+        /// <summary>
+        /// Constructor for the particle class
+        /// </summary>
+        /// <param name="x">The starting position on the x-axis</param>
+        /// <param name="y">The starting position on the y-axis</param>
         public particle(int x, int y)
         {
            
@@ -147,6 +170,7 @@ namespace drawing
             color = Color.FromArgb(a, rdm.Next(0, 255), rdm.Next(0, 255), rdm.Next(0, 255));
             brush = new SolidBrush(color);
         }
+
 
         public void update()
         {
